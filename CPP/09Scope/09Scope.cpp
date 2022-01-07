a0aa743f5b352a1c6fd1f4bc3807b8a9ca8f1027
yeomong0102 <yeomong01024411@gmail.com>
2022-01-07 13:50:52 +0900

    박지성 : 연산자 진짜 끝

---
+++ CPP/09Scope/09Scope.cpp
@@ -0,0 +1,23 @@
+﻿// 09Scope.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+int main()
+{// main 지역
+    // 실행흐름안에 실행흐름을 만들수 있다
+    
+    // 이름없는 지역
+    {
+        int Left = 0;
+        int Right = 0;
+        Left + Right;
+    }
+
+    {
+        int Left = 0;
+        int Right = 0;
+        Left - Right;
+    }
+}
+
