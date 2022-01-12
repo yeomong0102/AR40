defb4a3d5284cd6bbdd445508b442bb133aa80a8
yeomong0102 <yeomong01024411@gmail.com>
2022-01-12 16:27:14 +0900

    박지성: 반복문 조건반복문 숙제 커밋

---
+++ CPP/23if/23if.cpp
@@ -0,0 +1,50 @@
+﻿// 23if.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+void Func()
+{
+	int a = 0;
+
+	return;
+
+	int b = 0;
+}
+
+int main()
+{
+	Func();
+
+	int PlayerHp = 0;
+
+	// if ( 메모리 영역 )
+
+	// != 비교연산자가 반환하는것
+
+	// if ~ else if 몇개가 있더라도 
+	// 이 코드들은 하나의 연결된 세트로 보고
+	// 한개가 실행되면 나머지 코드들은 무시된다.
+
+	if (PlayerHp > 50)
+	{
+		int a = 0;
+	}
+	else if (PlayerHp > 30)
+	{
+		int a = 0;
+	}
+	else if (PlayerHp > 10)
+	{
+		int a = 0;
+	}
+	else 
+	{
+		int a = 0;
+	}
+
+
+	
+}
+
+
---
+++ CPP/26for/26for.cpp
@@ -0,0 +1,30 @@
+﻿// 26for.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+int main()
+{
+	int Value = 5;
+	{
+	
+
+	    while (0 != Value)
+	    {
+			// 감소
+			Value -= 1;
+	    }
+    }
+	{
+		for (
+			int Value = 0; // 초기화
+			Value < 10; // 조건문
+			Value += 1 // 증감문
+			)
+		{
+			int a = 0;
+		}
+	}
+}
+
+
