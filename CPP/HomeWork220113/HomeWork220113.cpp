70579324ef58e8b3c53c4e386a70f175891f0c9b
yeomong0102 <yeomong01024411@gmail.com>
2022-01-14 15:55:52 +0900

    박지성: 주말 숙제 끝

--- CPP/HomeWork220113/HomeWork220113.cpp
+++ CPP/HomeWork220113/HomeWork220113.cpp
@@ -3,6 +3,17 @@
 
 #include <iostream>
 
+//int StringCount(const char* _Left)
+//{
+//    int Count = 0;
+//    while (0 != _Left[Count])
+//    {
+//        Count += 1;
+//    }
+//
+//    return Count;
+//}
+
 void StringTrimRemove(const char* _Left, char* _Right)
 {
     const int OriginNum = 10000;
@@ -23,6 +34,26 @@ void StringTrimRemove(const char* _Left, char* _Right)
         }
     }
 
+    /*for (int LeftIndex = 0; 0 != _Left[LeftIndex]; LeftIndex += 1)
+    {
+        char ValueLeft = _Left[LeftIndex];
+        char ValueRight = _Right[RightIndex];
+
+        if (' ' != ValueLeft)
+        {
+
+           _Right[RightIndex] = _Left[LeftIndex];
+           RightIndex += 1;
+
+        }
+
+        _Right[RightIndex] = 0;
+
+        return;
+
+    }
+
+    */
 
 
     /*while (0 != _Right[Rcount])
@@ -49,9 +80,21 @@ void StringTrimRemove(const char* _Left, char* _Right)
 int main()
 {
 
+    {
+        // int aaa[4] = "a b"; <= int에서는 char에서 했던거 처럼 배열칸대신 문자열로 집어넣는게 안됨  
+
+        char Text0[5] = "a b";
+        char Text1[5] = { 0, };
+
+        // Text1안에 있는 배열 값을 Text0에다 집어 넣고 싶을때
+        // 일일이 Text0[5] = Text1[5]; 를 숫자만 바꿔서 치는게 아니라
+        // 반복문 for, while 같은 방식으로 하는게 일반적이다
+
+    }
+
     char Text[10000] = { 0 };
 
-    StringTrimRemove("aaa  bbb  ccc   ddd", Text);
+    StringTrimRemove("aaa  bbb  ccc   ddd adqsdqfasdq adasdqfadq      ", Text);
 
     // "aaabbbcccddd" 공백제거를 해주는 함수 만들기
     printf_s(Text);
