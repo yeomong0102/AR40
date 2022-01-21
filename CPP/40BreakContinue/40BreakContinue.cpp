241c2d26bf6543870a9d6b598448013fd6283182
yeomong0102 <yeomong01024411@gmail.com>
2022-01-21 17:08:29 +0900

    박지성: 텍스트 rpg 예제

---
+++ CPP/40BreakContinue/40BreakContinue.cpp
@@ -0,0 +1,55 @@
+﻿// 40BreakContinue.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+int main()
+{
+	int Arr[10] = { 10, 333, 222, 1, 231, 2323, 12 };
+	bool IsOne = false;
+    
+	// 반복문 내부에서 쓰이는 break는 switch문 내부에서와 쓰임새가 다르다
+	// 반복문에서는 break를 만나는순간 그 반복문을 종료하는 의미이다
+	// 반복문의 return같은 느낌
+
+	// 주의점은 해당 반복문만 나가는 것이기 때문에 
+
+	for (int i = 0; i < 10; i+= 1)
+	{
+		if (Arr[0] == 1)
+		{
+			// 가장 가까운 하나의 반복만 탈출이 가능
+			while (true)
+			{
+				IsOne = true;
+				break;
+			}
+
+			if (true == IsOne)
+			{
+				break;
+			}
+			
+		}
+	}
+
+	int a = 0;
+
+	int ArrTest[10] = {};
+	int Count = 0;
+	for (int i = 0; i < 20; i+= 1)
+	{
+		if (1 == (i % 2))
+		{
+			continue;
+		}
+
+		ArrTest[Count] = i;
+		Count += 1;
+	}
+
+	// while은 증감문이 없으므로
+	// 
+}
+
+
---
+++ CPP/TestTextRpg/TestTextRpg.cpp
@@ -0,0 +1,147 @@
+﻿// TestTextRpg.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+
+class Player
+{
+private:
+    int Att;
+    int Hp;
+    int Def;
+
+public:
+
+    bool IsDeath()
+    {
+        return Hp <= 0;
+    }
+
+    int GetAtt()
+    {
+        return Att;
+    }
+
+    void Damage(int _Damage)
+    {
+        printf_s("플레이어가");
+        char Arr[10] {0, };
+        _itoa_s(_Damage, Arr, 10);
+        printf_s(Arr);
+        printf_s("의 데미지를 입었습니다.\n");
+
+        Hp -= _Damage;
+    }
+public:
+    Player()
+        : Att(10)
+        , Hp(10000)
+        , Def(5)
+    {
+        int a = 0;
+    }
+};
+
+class Monster
+{
+private:
+    const char* Name;
+
+    int Att;
+    int Hp;
+    int Def;
+public:
+    
+    bool IsDeath()
+    {
+        return Hp <= 0;
+    }
+
+    int GetAtt()
+    {
+        return Att;
+    }
+
+    void Damage(int _Damage)
+    {
+        printf_s("몬스터가");
+        char Arr[10]{ 0, };
+        _itoa_s(_Damage, Arr, 10);
+        printf_s(Arr);
+        printf_s("의 데미지를 입었습니다.\n");
+        Hp -= _Damage;
+    }
+public:
+    Monster()
+        : Att(10)
+        , Hp(100)
+        , Def(1)
+    {
+        int a = 0;
+    }
+};
+
+class FightZone
+{
+public:
+    void Fight(Player& _Player, Monster& _Monster)
+    {
+        // if (nullptr == _Monster)
+        // {
+        //    return;
+        // }
+        while (true)
+        {
+            if (_Monster.IsDeath())
+            {
+                break;
+            }
+
+            _Monster.Damage(_Player.GetAtt());
+            _Player.Damage(_Monster.GetAtt());
+
+            
+        }
+        
+        int a = 0;
+    }
+public:
+    FightZone()
+    {
+
+    }
+};
+
+int main()
+{
+    Player MainPlayer = Player();
+    Monster MainMonster[10] = {"오크", "드래곤", "코볼트"};
+    FightZone NewFightZone = FightZone();
+
+    int MonsterIndex = 0;
+
+    while (true)
+    {
+        NewFightZone.Fight(MainPlayer, MainMonster[MonsterIndex]);
+
+        if (true == MainPlayer.IsDeath())
+        {
+            printf_s("플레이어가 패배했습니다.");
+        }
+
+        MonsterIndex += 1;
+
+        if (sizeof(Monster) / sizeof(MainMonster) <= MonsterIndex)
+        {
+            printf_s("플레이어가 승리했습니다. \n");
+            char Arr[10]{ 0, };
+            _itoa_s(MonsterIndex, Arr, 10);
+            printf_s(Arr);
+            printf_s("마리 몬스터를 죽였습니다.\n");
+            break;
+        }
+    }
+
+}
+
