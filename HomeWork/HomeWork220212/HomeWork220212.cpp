39f993ade698558521de6812d155bdcd7da2a016
yeomong0102 <yeomong01024411@gmail.com>
2022-02-12 12:59:22 +0900

    숙제

---
+++ HomeWork/HomeWork220212/HomeWork220212.cpp
@@ -0,0 +1,20 @@
+﻿// HomeWork220212.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+class Myint
+{
+public:
+    Myint& operator()() 
+    {
+        return *this;
+    }
+};
+
+int main()
+{
+    Myint NewA0;
+    Myint NewA1(NewA0);
+}
+
