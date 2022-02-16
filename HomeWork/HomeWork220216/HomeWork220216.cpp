a80055db5d80e1ec922b8db7aade70ee17d3f0ec
yeomong0102 <yeomong01024411@gmail.com>
2022-02-16 09:50:47 +0900

    숙제끗

---
+++ HomeWork/HomeWork220216/HomeWork220216.cpp
@@ -0,0 +1,117 @@
+﻿#include <iostream>
+#include <assert.h>
+#include <crtdbg.h>
+
+using DataType = int;
+class GameArray
+{
+private:
+    int Size_;
+    DataType* ArrData_;
+
+public:
+    int GetSize()
+    {
+        return Size_;
+    }
+
+    DataType& operator[](int _Index)
+    {
+        if (Size_ <= _Index)
+        {
+            assert(false);
+        }
+
+        return ArrData_[_Index];
+    }
+
+    inline void operator=(const GameArray& _Other)
+    {
+        ReSize(_Other.Size_);
+
+        for (size_t i = 0; i < this->Size_; i++)
+        {
+            this->ArrData_[i] = _Other.ArrData_[i];
+        }
+
+        return;
+    }
+
+    inline void AllValueSetting(const DataType& Data)
+    {
+        for (size_t i = 0; i < this->Size_; i++)
+        {
+            this->ArrData_[i] = Data;
+        }
+    }
+
+    inline void Release()
+    {
+        if (nullptr != ArrData_)
+        {
+            delete[] ArrData_;
+            ArrData_ = nullptr;
+        }
+    }
+
+    void ReSize(unsigned int _Size)
+    {
+        DataType* TempData = new DataType[_Size];
+
+        for (int i = 0; i < _Size; i++)
+        {
+            if (i < Size_)
+            {
+                TempData[i] = ArrData_[i];
+            }
+            else
+            {
+                TempData[i] = i;
+            }
+        }
+
+        Release();
+
+        ArrData_ = TempData;
+        Size_ = _Size;
+    }
+
+public:
+    GameArray(unsigned int _Size)
+        : ArrData_(nullptr)
+        , Size_(0)
+    {
+        ReSize(_Size);
+    }
+
+    ~GameArray()
+    {
+        Release();
+    }
+};
+
+int main()
+{
+    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+
+    GameArray NewArray = GameArray(10);
+    // GameArray NewArray2 = GameArray(5);
+    GameArray NewArray2 = GameArray(20);
+
+    int Size = sizeof(GameArray);
+
+    for (size_t i = 0; i < NewArray.GetSize(); i++)
+    {
+        NewArray[i] = i;
+    }
+
+    NewArray.AllValueSetting(0);
+    NewArray2 = NewArray;
+
+    // NewArray2.ReSize(20);
+
+    for (size_t i = 0; i < NewArray2.GetSize(); i++)
+    {
+        std::cout << NewArray2[i] << std::endl;
+    }
+}
