6531fe1e6c0559cb5e11c4728620c91d8e88ffee
yeomong0102 <yeomong01024411@gmail.com>
2022-02-23 18:01:18 +0900

    30개까지만 만들고 중단

---
+++ HomeWork/HomeWork220223/HomeWork220223.cpp
@@ -0,0 +1,24 @@
+﻿// HomeWork220223.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+
+int Value = 0;
+
+void Func()
+{
+
+    if (Value == 30)
+    {
+        return;
+    }
+    std::cout << ++Value << std::endl;
+
+    Func();
+}
+
+int main()
+{
+    Func();
+}
