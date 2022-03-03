2ab3d0e61c5063ee1892c33e8c72989a2c0a927d
박지성 <zzmars4411@gmail.com>
2022-03-03 19:15:21 +0900

    박지성: API 윈도우 창 띄우기

--- AR40API/GameApp/main.cpp
+++ AR40API/GameApp/main.cpp
@@ -1,11 +1,13 @@
 #include <Windows.h>
 
-
+#include <GameEngineBase/GameEngineWindow.h>
 
 int __stdcall WinMain(_In_ HINSTANCE hInstance,
     _In_opt_ HINSTANCE hPrevInstance,
     _In_ char* lpCmdLine,
     _In_ int       nCmdShow)
 {
-    
+    // GameEngineBase의 cpp까지 모르기때문에
+    GameEngineWindow::GetInst().CreateGameWindow();
+    GameEngineWindow::GetInst().ShowGameWindow();
 }
--- AR40API/GameEngineBase/GameEngineWindow.cpp
+++ AR40API/GameEngineBase/GameEngineWindow.cpp
@@ -1,1 +1,51 @@
 #include "GameEngineWindow.h"
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    return DefWindowProc(hWnd, message, wParam, lParam);
+}
+
+GameEngineWindow* GameEngineWindow::Inst_ = new GameEngineWindow;
+
+GameEngineWindow::GameEngineWindow() 
+{
+}
+
+GameEngineWindow::~GameEngineWindow() 
+{
+}
+
+void GameEngineWindow::CreateGameWindow(HINSTANCE _hInst)
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc = WndProc;
+    wcex.cbClsExtra = 0;
+    wcex.cbWndExtra = 0;
+    wcex.hInstance = _hInst;
+    wcex.hIcon = nullptr;
+    wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 2);
+    wcex.lpszMenuName = nullptr;
+    wcex.lpszClassName = L"GameEngineWindowClass";
+    wcex.hIconSm = nullptr;
+
+    RegisterClassExW(&wcex);
+
+    hWnd_ = CreateWindowW(L"GameEngineWindowClass", L"packman", WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, _hInst, nullptr);
+
+    if (!hWnd_)
+    {
+        return;
+    }
+}
+
+void GameEngineWindow::ShowGameWindow()
+{
+
+}
+
